c4ef641333f1ae91bcba33b88853dff5a91fb5dc
link717 <ksy95433@gmail.com>
2022-10-17 11:51:31 +0900

    L2: Creating the Vue App

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-const product = 'Socks'
+// createApp 함수는 view와 js를 연결해주는 다리 역할을 한다.
+const app = Vue.createApp({
+  data() {
+    return {
+      product: 'Socks',
+      description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    };
+  },
+});
