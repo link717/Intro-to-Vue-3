03d65b7d8fea7b14ea87b9bdf852710533710974
link717 <ksy95433@gmail.com>
2022-10-17 12:32:27 +0900

    L5: List rendering

--- main.js
+++ main.js
@@ -4,9 +4,21 @@ const app = Vue.createApp({
     return {
       product: 'Socks',
       image: './assets/images/socks_green.jpg',
-      //   inStock: false,
-      inventory: 10,
-      onSale: true,
+      inStock: true,
+      //   inventory: 10,
+      //   onSale: true,
+      details: ['50% cotton', '30% wool', '20% polyester'],
+      variants: [
+        {
+          id: '2234',
+          color: 'green',
+        },
+        {
+          id: '2235',
+          color: 'blue',
+        },
+      ],
+      sizes: [230, 240, 250],
     };
   },
 });
